37c79691cd5c1b98a93efba14a80935f9b027c0d
tisha077 <kurshpetov@gmail.com>
2017-07-10 11:14:55 +0300

    login & register form

--- resources/views/frontend/pages/collections/index.blade.php
+++ resources/views/frontend/pages/collections/index.blade.php
@@ -27,32 +27,26 @@
         <div class="zon-col-list clearfix">
             @foreach($collections as $collection)
                 @if($collection->banner == 0)
-                    <div class="item-coll zon-col"><a
-                                href="{{ route('frontend.collections.show', $collection) }}">
-                            <div class="wrap-img-bg small">
-                                <div class="img-back wave-dark">
-                                    <svg width=1395.63 height=1237.68>
-                                        <use xlink:href=wave.svg#wave></use>
-                                    </svg>
-                                </div>
-                                <img src="/upload/images/{{ $collection->image }}" alt=""></div>
-                            <div>
-                                <div class=coll-name>
-                                    @if (App::getLocale() == 'ru')
-                                        {{ $collection->title_ru }}
-                                    @elseif(App::getLocale() == 'it')
-                                        {{ $collection->title_it }}
-                                    @else
-                                        {{ $collection->title }}
-                                    @endif
-                                    <span class=wrap-coll-name-arrow><span
-                                                class=coll-name-arrow>→</span></span></div>
-                                <div class=numb-prod>69 {{ trans('frontend.zones-collections.products') }}</div>
+                <div class="item-coll zon-col">
+                    <a href="{{ route('frontend.collections.show', $collection) }}">
+                        <div class="wrap-img-bg small">
+                            <div class="img-back wave-dark">
+                                <svg width=1395.63 height=1237.68>
+                                    <use xlink:href=wave.svg#wave></use>
+                                </svg>
                             </div>
-                        </a></div>
+                            <img src="/upload/images/{{ $collection->image }}" alt=""></div>
+                        <div>
+                            <div class=coll-name>
+                                {{ $collection->{'title'.$langSuf} }}
+                                <span class=wrap-coll-name-arrow><span
+                                            class=coll-name-arrow>→</span></span></div>
+                            <div class=numb-prod>69 {{ trans('frontend.zones-collections.products') }}</div>
+                        </div>
+                    </a>
+                </div>
                 @endif
             @endforeach
-
         </div>
     </div>
 </section>
